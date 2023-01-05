ccafbc8ea635218f5ca9494b08a612911fa99c72
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-01-05 10:26:48 -0700

    js fixed

--- main.js
+++ main.js
@@ -7,6 +7,19 @@ let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
 
-recognition.addEventListener('results', e => {
+recognition.addEventListener('result', e => {
     console.log(e)
+    const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('')
+
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
 });
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
