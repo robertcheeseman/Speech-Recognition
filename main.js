57c78c2f7d16b2ec48ddfc81148fb837a667e444
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2023-01-04 22:18:53 -0700

    update

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('results', e => {
+    console.log(e)
+});
